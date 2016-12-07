24139b785d2732d8d521ea998a36220c5894c831
fanuel nunes <fanuelson@gmail.com>
2016-12-07 19:40:03 -0200

    Refatorações

--- consumacao/ConsultaConsumacaoClienteController.js
+++ consumacao/ConsultaConsumacaoClienteController.js
@@ -1,80 +1,80 @@
 function consultaConsumacaoClienteController($scope, APP_CONFIG, $stateParams, clienteService, bonusService
-	, consumacaoService
-	,toastr) {
+    , consumacaoService
+    ,toastr) {
 
-	$scope.headerMessage = "Consumações de Cliente";
+    $scope.headerMessage = "Consumações de Cliente";
 
-	$scope.formFornecedorLoading = false;
+    $scope.formFornecedorLoading = false;
 
-	$scope.pageSize = APP_CONFIG.DEFAULT_PAGE_SIZE;
+    $scope.pageSize = APP_CONFIG.DEFAULT_PAGE_SIZE;
 
-	$scope.cliente = $stateParams.cliente;
+    $scope.cliente = $stateParams.cliente;
 
-	$scope.consumacoes = {};
-	$scope.consumacoesPage = 1;
+    $scope.consumacoes = {};
+    $scope.consumacoesPage = 1;
 
-	$scope.bonusCliente = {};
-	$scope.bonusClientePage = 1;
+    $scope.bonusCliente = {};
+    $scope.bonusClientePage = 1;
 
-	$scope.darBonusRequest = {};
+    $scope.darBonusRequest = {};
 
-	$scope.findAllConsumacoesByCliente = function() {
-		var $promise = clienteService.findAllConsumacoesByCliente($scope.cliente.id, $scope.consumacoesPage - 1 , $scope.pageSize);
-		$promise.success(function(res){
-			$scope.consumacoes = res;
+    $scope.findAllConsumacoesByCliente = function() {
+        var $promise = clienteService.findAllConsumacoesByCliente($scope.cliente.id, $scope.consumacoesPage - 1 , $scope.pageSize);
+        $promise.success(function(res){
+            $scope.consumacoes = res;
 
-		}).error(function(res){
-			console.log(JSON.stringify(res));
-		});
-	}
+        }).error(function(res){
+            console.log(JSON.stringify(res));
+        });
+    }
 
-	$scope.findAllBonusByCliente = function() {
-		var $promise = clienteService.findAllBonusByCliente($scope.cliente.id, $scope.bonusClientePage - 1, $scope.pageSize);
-		$promise.success(function(res){
-			$scope.bonusCliente = res;
-		}).error(function(res){
-			console.log(JSON.stringify(res));
-		});
-	}
+    $scope.findAllBonusByCliente = function() {
+        var $promise = clienteService.findAllBonusByCliente($scope.cliente.id, $scope.bonusClientePage - 1, $scope.pageSize);
+        $promise.success(function(res){
+            $scope.bonusCliente = res;
+        }).error(function(res){
+            console.log(JSON.stringify(res));
+        });
+    }
 
-	$scope.darBonus = function(idProd) {
+    $scope.darBonus = function(idProd) {
 
-		$scope.darBonusRequest = {
-			idCliente : $scope.cliente.id,
-			idProduto : idProd
-		}
-		$promiseDarBonus = bonusService.darBonus($scope.darBonusRequest);
-		$promiseDarBonus.success(function(res){
-			$scope.findAllBonusByCliente();
-		}).error(function(res){
+        $scope.darBonusRequest = {
+            idCliente : $scope.cliente.id,
+            idProduto : idProd
+        }
+        $promiseDarBonus = bonusService.darBonus($scope.darBonusRequest);
+        $promiseDarBonus.success(function(res){
+            $scope.findAllBonusByCliente();
+        }).error(function(res){
 
 
-		})
-	}
+        })
+    }
 
-	$scope.pagarDivida = function(idCons) {
-		$promise = consumacaoService.pagarDivida(idCons);
-		$promise.success(function(res){
-			toastr.success(res.mensagem);
-			$scope.findAllConsumacoesByCliente();
-			$scope.findAllBonusByCliente();
-		}).error(function(res){
-			angular.forEach(res.validacoesRegraNegocio, function(value, key) {
-				toastr.error(value);
-			})
-		})
-	}
+    $scope.pagarDivida = function(idCons) {
+        $promise = consumacaoService.pagarDivida(idCons);
+        $promise.success(function(res){
+            toastr.success(res.mensagem);
+            $scope.findAllConsumacoesByCliente();
+            $scope.findAllBonusByCliente();
+        }).error(function(res){
+            angular.forEach(res.validacoesRegraNegocio, function(value, key) {
+                toastr.error(value);
+            })
+        })
+    }
 
 }
 
 var depends = [
     '$scope',
-	 'APP_CONFIG',
-	 '$stateParams',
+    'APP_CONFIG',
+    '$stateParams',
     'clienteService',
-	 'bonusService',
-	 'consumacaoService',
-	 'toastr',
+    'bonusService',
+    'consumacaoService',
+    'toastr',
     consultaConsumacaoClienteController ]
 
 angular.module('myApp').controller('consultaConsumacaoClienteController', depends);
